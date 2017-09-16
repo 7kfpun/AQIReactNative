7ca0fe898d0061244707f5b745f702cc7902c08d
kf <7kfpun@gmail.com>
2017-09-16 23:04:47 +0800

    Add simplified chinese location names

--- app/utils/locations.js
+++ app/utils/locations.js
@@ -3,111 +3,143 @@ exports.locations = [{
     latitude: 22.286374,
     longitude: 114.142542,
   },
+  key: 'central_western',
   title: 'Central/Western',
   title_hant: '中西區',
+  title_hans: '中西区',
 }, {
   latlng: {
     latitude: 22.315698,
     longitude: 114.233106,
   },
+  key: 'kwun_tong',
   title: 'Kwun Tong',
   title_hant: '觀塘',
+  title_hans: '观塘',
 }, {
   latlng: {
     latitude: 22.442322,
     longitude: 114.165506,
   },
+  key: 'tai_po',
   title: 'Tai Po',
   title_hant: '大埔',
+  title_hans: '大埔',
 }, {
   latlng: {
     latitude: 22.287374,
     longitude: 113.942509,
   },
+  key: 'tung_chung',
   title: 'Tung Chung',
   title_hant: '東涌',
+  title_hans: '东涌',
 }, {
   latlng: {
     latitude: 22.273389,
     longitude: 114.236078,
   },
+  key: 'eastern',
   title: 'Eastern',
   title_hant: '東區',
+  title_hans: '东区',
 }, {
   latlng: {
     latitude: 22.377130,
     longitude: 114.197440,
   },
+  key: 'sha_tin',
   title: 'Sha Tin',
   title_hant: '沙田',
+  title_hans: '沙田',
 }, {
   latlng: {
     latitude: 22.311936,
     longitude: 114.256878,
   },
+  key: 'tseung_kwan_o',
   title: 'Tseung Kwan O',
   title_hant: '將軍澳',
+  title_hans: '将军澳',
 }, {
   latlng: {
     latitude: 22.475460,
     longitude: 114.359399,
   },
+  key: 'tap_mun',
   title: 'Tap Mun',
   title_hant: '塔門',
+  title_hans: '塔门',
 }, {
   latlng: {
     latitude: 22.444548,
     longitude: 114.022210,
   },
+  key: 'yuen_long',
   title: 'Yuen Long',
   title_hant: '元朗',
+  title_hans: '元朗',
 }, {
   latlng: {
     latitude: 22.390829,
     longitude: 113.972513,
   },
+  key: 'tuen_mun',
   title: 'Tuen Mun',
   title_hant: '屯門',
+  title_hans: '屯门',
 }, {
   latlng: {
     latitude: 22.366850,
     longitude: 114.130259,
   },
+  key: 'kwai_chung',
   title: 'Kwai Chung',
   title_hant: '葵涌',
+  title_hans: '葵涌',
 }, {
   latlng: {
     latitude: 22.332093,
     longitude: 114.146908,
   },
+  key: 'sham_shui_po',
   title: 'Sham Shui Po',
   title_hant: '深水埗',
+  title_hans: '深水埗',
 }, {
   latlng: {
     latitude: 22.371323,
     longitude: 114.114160,
   },
+  key: 'tsuen_wan',
   title: 'Tsuen Wan',
   title_hant: '荃灣',
+  title_hans: '荃湾',
 }, {
   latlng: {
     latitude: 22.285979,
     longitude: 114.191492,
   },
+  key: 'causeway_bay',
   title: 'Causeway Bay',
   title_hant: '銅鑼灣',
+  title_hans: '铜锣湾',
 }, {
   latlng: {
     latitude: 22.279991,
     longitude: 114.158798,
   },
+  key: 'central',
   title: 'Central',
   title_hant: '中環',
+  title_hans: '中环',
 }, {
   latlng: {
     latitude: 22.320365,
     longitude: 114.169773,
   },
+  key: 'mong_kok',
   title: 'Mong Kok',
   title_hant: '旺角',
+  title_hans: '旺角',
 }];
